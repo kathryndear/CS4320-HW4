5c75d10d8ff562cb2e1e9ab9884f7b6cec7aedde
Katie Dear <kathrynmdear@gmail.com>
2016-04-07 18:35:22 -0400

    with edits/comments from office hours

---
+++ src/TrustReducer (copy).java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.util.*;
+
+public class TrustReducer extends Reducer<IntWritable, NodeOrDouble, IntWritable, Node> {
+    public void reduce(IntWritable key, Iterable<NodeOrDouble> values, Context context)
+        throws IOException, InterruptedException {
+        //Implement
+	
+	Node m = null;
+	Iterator<NodeOrDouble> it = values.iterator();
+
+	double sum = 0;
+	while (it.hasNext()) {
+		NodeOrDouble nod = it.next();
+		if (nod.isNode()) {
+			m = nod.getNode();
+		}
+		else {
+			sum = sum + nod.getDouble();
+		}
+	}
+
+	m.setPageRank(sum);
+	context.write(key,m);
+
+    }
+}
